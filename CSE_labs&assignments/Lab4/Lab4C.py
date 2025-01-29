90482b3c79482e314422632b6c6b1ffc9908569b
V_eneno <37306118+Willco9960@users.noreply.github.com>
2025-01-28 21:35:52 -0500

    Create Lab4C.py

---
+++ CSE_labs&assignments/Lab4/Lab4C.py
@@ -0,0 +1,28 @@
+print(f"Welcome!")
+num = float(input(f"Please input a number: "))
+
+print()
+
+print(f"What would you like to do with this number:")
+print(f"0) Get the additive inverse of the number")
+print(f"1) Get the reciprocal of the number")
+print(f"2) Square the number")
+print(f"3) Cube the number")
+print(f"4) Exit the program")
+op = int(input())
+
+if op == 0:
+    print(f"The additive inverse of {num} is {-num}")
+if op == 1:
+    if num == 0:
+        print(f"Cannot divide by 0!")
+    else:
+        print(f"The reciprocal of {num} is", round(num ** -1, 3))
+elif op == 2:
+    print(f"The square of {num} is", (num ** 2))
+elif op == 3:
+    print(f"The cube of {num} is", (num ** 3))
+elif op == 4:
+    print(f"Thank you, goodbye!")
+else:
+    print(f"Invalid option!")
