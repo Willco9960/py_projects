4bdc60b6909ca612f367e8d21501ddac8906ec4b
V_eneno <37306118+Willco9960@users.noreply.github.com>
2025-01-28 21:35:05 -0500

    Create Lab4A.py

---
+++ CSE_labs&assignments/Lab4/Lab4A.py
@@ -0,0 +1,28 @@
+grade = float(input(f"Enter your grade:"))
+
+if grade > 97:
+    print(f"Letter grade is: A+")
+elif grade > 94:
+    print(f"Letter grade is: A")
+elif grade > 91:
+    print(f"Letter grade is: A-")
+elif grade > 88:
+    print(f"Letter grade is: B+")
+elif grade > 85:
+    print(f"Letter grade is: B")
+elif grade > 82:
+    print(f"Letter grade is: B-")
+elif grade > 79:
+    print(f"Letter grade is: C+")
+elif grade > 76:
+    print(f"Letter grade is: C")
+elif grade > 73:
+    print(f"Letter grade is: C-")
+elif grade > 70:
+    print(f"Letter grade is: D+")
+elif grade > 67:
+    print(f"Letter grade is: D")
+elif grade > 64:
+    print(f"Letter grade is: D-")
+elif grade < 64:
+    print(f"Letter grade is: F")
